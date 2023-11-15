c0423f29584dcccdfe8cde548c75af6d7c0dd7eb
Aswin Lal M <aswnss.m@gmail.com>
2023-11-15 08:10:09 +0530

    updates

--- app/api/test/route.tsx
+++ app/api/test/route.tsx
@@ -1,13 +1,29 @@
-export async function POST(request : Request){
-    if(request.method == "POST"){
+import { client } from "../server"
+
+export async function GET(request: Request, response: Response) {
+    try {
+        const results = await client.("SELECT * FROM subscribers;")
         return Response.json({
-            status : 200,
-            message : "Connection Successfull"
+            status: response.status,
+            results: {
+                oid: results.oid, //? I dont know what this is .
+                command: results.command,
+                rowCount: results.rowCount,
+                rows: results.rows,
+                fields: results.fields, // columns
+            }
         })
-    }else{
-        return Response.json({
-            status: 405,
-            message : "Method Not Allowed"
-        })
+    } catch (err) {
+        console.log(err)
     }
+    return Response.json({
+        status: response.status
+    })
+}
+
+export async function POST(request: Request) {
+    return Response.json({
+        status: 200,
+        message: "Post request recievied"
+    })
 }
